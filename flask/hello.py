64705b536e4ffcd255dd65853a2d0d6c5a139eef
dursss <it.duruishan@gmail.com>
2017-07-04 18:04:51 +0800

    quick start

--- flask/hello.py
+++ flask/hello.py
@@ -2,8 +2,32 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+def main():
+    return "这是首页"
+
+@app.route("/index")
 def hello():
     return "Hello World!"
+
+@app.route("/hello")
+def helloworld():
+    return "欢迎界面"
+
+@app.route("/user/<username>")
+def show_user_profile(username):
+    return "User %s" % username
+
+@app.route("/post/<int:post_id>")
+def show_post(post_id):
+    return "post %d" % post_id
+
+@app.route("/projects/")
+def projects():
+    return "The project page"
+
+@app.route("/about")
+def about():
+    return "The about page"
 
 if __name__ == "__main__":
     app.run()
